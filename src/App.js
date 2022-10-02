f8a0ca66dd017167173b0042ce7d58e3215f3640
cmskorcz <chaseskorey@icloud.com>
2022-10-02 08:46:08 -0400

    added state functions for modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react'
 import Navigation from "./components/Navigation";
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleCloseModal = () => setShowModal(false)
+  const handleShowModal = () => setShowModal(true)
+
   return (
     <div className="App">
-      <Navigation />
+      <Navigation 
+        showModal={showModal} 
+        handleShowModal={handleShowModal}
+        handleCloseModal={handleCloseModal}  
+      />
     </div>
   );
 }
