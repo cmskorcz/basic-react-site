790e4596aa73a1ca7eae1fd1bc9b1e116668ed59
cmskorcz <chaseskorey@icloud.com>
2022-10-02 22:59:34 -0400

    routes on navigation bar work to change page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 import ContactModal from './components/ContactModal';
 import Navigation from "./components/Navigation";
+import About from './pages/About';
+import Home from './pages/Homepage';
+import Products from './pages/Products';
 
 function App() {
   const [showModal, setShowModal] = useState(false)
@@ -18,6 +26,13 @@ function App() {
         showModal={showModal}
         handleCloseModal={handleCloseModal}
       />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
