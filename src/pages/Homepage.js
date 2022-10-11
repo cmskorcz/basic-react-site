7042b13c9a06f4b66eaacc9fd2aac225ae3991b8
cmskorcz <chaseskorey@icloud.com>
2022-10-11 16:43:20 -0400

    added side image

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -18,6 +18,12 @@ function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien faucibus et molestie ac feugiat sed lectus. Consequat mauris nunc congue nisi vitae suscipit tellus. Augue neque gravida in fermentum. Porta nibh venenatis cras sed. Ultricies lacus sed turpis tincidunt. Ultrices tincidunt arcu non sodales neque sodales. Dolor purus non enim praesent elementum facilisis. Lorem ipsum dolor sit amet consectetur adipiscing. Dolor sit amet consectetur adipiscing. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor.
           </p>
         </Col>
+        <Col lg={4}>
+          <img 
+            alt='side image' 
+            className='w-100 h-100 mb-3'
+          />
+        </Col>
       </Row>
     </Container>
   )
