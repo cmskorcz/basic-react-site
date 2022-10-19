f4dda08cf9c370b4f12e07ed5c87a0f7d25ef677
cmskorcz <chaseskorey@icloud.com>
2022-10-19 01:31:44 -0400

    services change on list click

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,4 +1,5 @@
 import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup'
 import { useSearchParams } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -7,19 +8,30 @@ function Services(props) {
   const [ searchParams, setSearchParams ] = useSearchParams();
   const [ serviceId, setServiceId ] = useState(searchParams.get('id'));
 
+  function handleServiceChange (e) {
+    setSearchParams(`id=${e.target.id}`);
+    setServiceId(e.target.id)
+  }
+
   return (
     <Container>
-      <h1>This is the Service page { serviceId }</h1>
+      <h1>Please Select a Service to View</h1>
+      <ListGroup>
+        { serviceArr.map(service => {
+          return (
+            <ListGroup.Item action id={service.id} key={service.id} onClick={handleServiceChange}>{service.title}</ListGroup.Item>
+          )
+        })}
+      </ListGroup>
       { serviceArr.map(service => {
         return (
           service.id === serviceId && 
-            <div>
+            <div key={service.id}>
               <h2>{ service.title }</h2>
               <p>{ service.description }</p>
             </div>
         )
       })}
-      <h2>Please Select a Service to View</h2>
     </Container>
   )
 }
