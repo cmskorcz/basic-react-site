302e5c8e9af91468dddc5f1aa1ead869d9b21838
cmskorcz <chaseskorey@icloud.com>
2022-10-23 13:35:23 -0400

    added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,32 @@ function Footer () {
         <MDBRow>
           <MDBCol lg={6} md={12} className='mb-4 mb-md-0'>
             <h5 className='text-uppercase'>Footer Content</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est
+              atque cumque eum delectus sint!
+            </p>
+          </MDBCol>
+
+          <MDBCol lg={3} md={6} className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Links</h5>
+            <ul className='list-unstyled mb-0'>
+              <li>
+                <a href='/' className='text-decoration-none text-black'>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='/about' className='text-decoration-none text-black'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href='/services' className='text-decoration-none text-black'>
+                  Services
+                </a>
+              </li>
+            </ul>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
